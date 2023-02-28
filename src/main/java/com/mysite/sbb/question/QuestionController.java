6799b45a93caa326f8753e82ebbaa3002cc4f883
yonghlee <cjswl1357@gmail.com>
2023-02-28 14:33:15 +0900

    Question Service 구조로 변경

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -11,12 +11,12 @@ import java.util.List;
 @RequiredArgsConstructor
 public class QuestionController {
 
-    private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
 
     @GetMapping("/question/list")
     public String list(Model model)
     {
-        List<Question> questionList = this.questionRepository.findAll();
+        List<Question> questionList = this.questionService.getList();
         // 모델 객체는 자바 클래스와 템플릿 간의 연결고리 역활을 한다. 템플릿에서 그 값을 쓸 수 있다.
         model.addAttribute("questionList", questionList);
         return "question_list";

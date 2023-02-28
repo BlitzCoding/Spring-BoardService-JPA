0ed1e6fca496e85637aa179668f2e7657fe984ce
yonghlee <cjswl1357@gmail.com>
2023-02-28 14:13:24 +0900

    엔티티 셋팅

---
+++ src/main/java/com/mysite/sbb/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.mysite.sbb;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> { // 리포지터리 대상이 되는 엔티티 타입과 해당 엔티티의PK의 속성타입을 지정
+
+}

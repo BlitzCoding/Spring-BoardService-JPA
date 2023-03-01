719857dd5e99182560342aa334e3baf27e03174d
yonghlee <cjswl1357@gmail.com>
2023-03-01 16:09:10 +0900

    로그인, 회원가입, 로그아웃 구현

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -1,7 +1,9 @@
 package com.mysite.sbb;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class MainController {
 
     @GetMapping("/")
